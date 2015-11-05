723c6fa5324fa3110179f16643d49cd88468fbef
Laurent THIEBAULT <thiebault.laurent@gmail.com>
2015-11-05 17:00:43 +0100

    Modification nom variable

--- PoolProject/src/lille1/pool/ResourcePool.java
+++ PoolProject/src/lille1/pool/ResourcePool.java
@@ -9,24 +9,24 @@ import lille1.pool.resource.Resource;
 public abstract class ResourcePool <T extends Resource>{
 	
 	protected int nbResources;
-	protected List<T> resources;
+	protected List<T> availableResources;
 	protected List<T> usedResources = new ArrayList<T>();
 	
 	public ResourcePool(int nbResources) {
 		this.nbResources = nbResources;
-		this.resources = new ArrayList<T>();
+		this.availableResources = new ArrayList<T>();
 		for (int i = 0; i < nbResources; i++) {
-			this.resources.add(create());
+			this.availableResources.add(create());
 		}
 	}
 	
 	protected abstract T create();
 	
 	public T provideResource() {
-		if (!resources.isEmpty()){
-			T tmp = resources.get(0);
+		if (!availableResources.isEmpty()){
+			T tmp = availableResources.get(0);
 			usedResources.add(tmp);
-			resources.remove(0);
+			availableResources.remove(0);
 			return tmp;
 		} else {
 			throw new NoSuchElementException();
@@ -35,7 +35,7 @@ public abstract class ResourcePool <T extends Resource>{
 	
 	public void freeResource(T resource) {
 		if (usedResources.contains(resource)){
-			resources.add(resource);
+			availableResources.add(resource);
 			usedResources.remove(resource);
 		}
 		else {
@@ -44,11 +44,11 @@ public abstract class ResourcePool <T extends Resource>{
 	}
 
 	public List<T> getResources() {
-		return resources;
+		return availableResources;
 	}
 
 	public void setResources(List<T> resources) {
-		this.resources = resources;
+		this.availableResources = resources;
 	}
 
 	public List<T> getUsedResources() {
