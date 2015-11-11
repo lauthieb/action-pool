4549159d4da8a5b44384b0f357da20935e0012e8
crastchet <crastchet@gmail.com>
2015-11-11 16:09:48 +0100

    Ajout javadoc dans FairScheduler

--- PoolProject/src/lille1/action/scheduler/FairScheduler.java
+++ PoolProject/src/lille1/action/scheduler/FairScheduler.java
@@ -2,8 +2,18 @@ package lille1.action.scheduler;
 
 import lille1.action.exception.ActionFinishedException;
 
+/**
+ * FairScheduler is a scheduler which execute actions in parallel.
+ * It executes an action and goes the next one which is not finished.  
+ * @author thibault
+ *
+ */
 public class FairScheduler extends Scheduler {
 
+	/**
+	 * Execute the action which follows the last action executed.
+	 * If the action we want to execute is already finished, we execute the first next one which is not finished. 
+	 */
 	@Override
 	public void reallyDoStep() throws ActionFinishedException {
 		if(this.currentAction >= schedulerActions.size()) {
